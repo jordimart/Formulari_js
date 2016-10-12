97bd6d55b9239e82fa15adfa3f993d9f1f29f114
Jorge <jordimart83@gmail.com>
2016-10-12 21:01:45 +0200

    Actualización en la parte de validar, pero me fallan las expresiones regulares

--- javascript/validate.js
+++ javascript/validate.js
@@ -9,23 +9,100 @@ function Validate() {
 
     // console.log("estoy dentro de update");
 
+    // variables de tipo para cambiar su estado
+    var differential_type = document.getElementById('differential');
+    var euribor_type = document.getElementById('euribor');
+    var fixed_interest_type = document.getElementById('fixed_interest');
+    // variables de valor
     var ingresos_mensuales =
       document.getElementById('ingresos_mensuales').value;
     var capital = document.getElementById('capital').value;
     var interest_rate_type =
       document.getElementById('interest_rate_type').value;
     var euribor = parseFloat(document.getElementById('euribor').value);
-    var euribor_type = document.getElementById('euribor');
     var differential =
       parseFloat(document.getElementById('differential').value);
-    var differential_type = document.getElementById('differential');
     var fixed_interest =
       parseFloat(document.getElementById('fixed_interest').value);
-    var fixed_interest_type = document.getElementById('fixed_interest');
     var terminiAnys = document.getElementById('period').value;
 
     var monthly_quote = document.getElementById('monthlyQuote');
     var interesAplicat = document.getElementById('interestApplied');
+
+    function pass(terminiAnys, capital, ingresos_mensuales, euribor,
+      differential, fixed_interest) {
+      var result = true;
+
+      // var num = /^[0-9]+([.][0-9]+)?$/;
+      // var porcentage = /^[0-9]\d{1,2}(\.\d{1,2})?$/;
+      // var money = /^[1-9]\d{1,7}(?:\.\d{1,4})?$/;
+      var porcentage = /[0-9]/;
+      var money = /[0-9]/;
+
+      if (ingresos_mensuales === "") {
+        result = false;
+      } else if (!money.test(ingresos_mensuales)) {
+        result = false;
+      }
+
+      if (capital === "") {
+        result = false;
+      } else if (!money.test(capital)) {
+        result = false;
+      }
+
+      // validaciones campos de porcentajes
+      if (!euribor_type.readOnly) {
+        if (!porcentage.test(euribor)) {
+          result = false;
+        }
+      }
+      if (!differential_type.readOnly) {
+        if (!porcentage.test(differential)) {
+          result = false;
+        }
+      }
+      if (!fixed_interest_type.readOnly) {
+        if (!porcentage.test(fixed_interest)) {
+          result = false;
+        }
+      }
+
+      if (terminiAnys === "") {
+        result = false;
+      } else if ((terminiAnys < 5) || (terminiAnys > 50)) {
+        result = false;
+      }
+
+      console.log(result);
+      return result;
+    }
+
+    // función para sumar las rebajas por productos escogidos
+    function check_box() {
+      var checkboxes = document.getElementById("formulario_products").checkbox;
+      var cont = 0;
+      var suma = 0;
+      for (var x = 0; x < checkboxes.length; x++) {
+        if (checkboxes[x].checked) {
+          cont = cont + 1;
+        }
+      }
+      if (cont === 1) {
+        suma = 0.25;
+      } else if (cont === 2) {
+        suma = 0.50;
+      } else if (cont === 3) {
+        suma = 0.75;
+      } else {
+        suma = 0;
+      }
+
+      return suma;
+    }
+
+    // aplicamos la funcion para que nos diga la rebaja
+    var int = check_box();
 
     // En esta condición decidimos que campos serán editables dependiendo del
     // tipo
@@ -35,16 +112,17 @@ function Validate() {
       fixed_interest_type.readOnly = true;
       euribor_type.readOnly = false;
       differential_type.readOnly = false;
-      interesAplicat.value = (euribor) + (differential);
+      interesAplicat.value = euribor + differential - int;
     } else {
       // console.log("interes no variable");
       euribor_type.readOnly = true;
       differential_type.readOnly = true;
       fixed_interest_type.readOnly = false;
-      interesAplicat.value = (fixed_interest);
+      interesAplicat.value = fixed_interest - int;
     }
 
-    var result = true;
+    var result = pass(terminiAnys, capital, ingresos_mensuales, euribor,
+      differential, fixed_interest);
     if (result) {
       console.log("estoy dentro del if de imprimir");
       var total = parseFloat((capital * interesAplicat.value) / 12) /
